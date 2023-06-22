0cb68e1fc46b32a0fc0c1bddd978732556fd9c35
Wahyu Anggara Raya <wahyu.anggara@hiaria.id>
2023-06-22 12:06:26 +0700

    feat: send data back to previous screen

--- lib/pages/todo/todo_pages.dart
+++ lib/pages/todo/todo_pages.dart
@@ -15,20 +15,103 @@ class TodoPages extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Todo"),
       ),
-      body: ListView.builder(
-          itemCount: list.length,
-          itemBuilder: (context, index) => ListTile(
-                title: Text(list[index].title),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => DetailScreen(todo: list[index]),
-                          settings: RouteSettings(
-                            arguments: list[index],
-                          )));
-                },
-              )),
+      body: TodoList(todoList: list)
+    );
+  }
+}
+
+class TodoList extends StatefulWidget {
+  const TodoList({super.key, required this.todoList});
+
+  final List todoList;
+
+  @override
+  State<TodoList> createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: widget.todoList.length,
+        itemBuilder: (context, index) => ListTile(
+              title: Text(widget.todoList[index].title),
+              onTap: () {
+                _navigateToDetailScreen(context, widget.todoList[index]);
+              },
+            ));
+  }
+
+  Future<void> _navigateToDetailScreen(BuildContext context, Todo todo) async {
+    // Navigator.push returns a Future that completes after calling
+    // Navigator.pop on the Selection Screen.
+    final result = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => DetailScreen(todo: todo),
+            settings: RouteSettings(
+              arguments: todo,
+            )));
+    // When a BuildContext is used from a StatefulWidget, the mounted property
+    // must be checked after an asynchronous gap.
+    if (!mounted || result == null) return;
+
+    // After the Selection Screen returns a result, hide any previous snackbars
+    // and show the new result.
+    ScaffoldMessenger.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(SnackBar(content: Text('$result')));
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  // Redundant, only to display another option to send data besides ModalRoute
+  const DetailScreen({super.key, required this.todo});
+
+  // Declare a field that holds the Todo.
+  final Todo todo;
+
+  // The navigation arrow appears automatically when a prev route is available
+  @override
+  Widget build(BuildContext context) {
+    final todoArg = ModalRoute.of(context)!.settings.arguments as Todo;
+    // Use the Todo to create the UI.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todoArg.title),
+      ),
+      body: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(todoArg.description),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Close the screen and return "Yep!" as the result.
+                    Navigator.pop(context, 'Yep! => ${todoArg.title}');
+                  },
+                  child: const Text('Yep!'),
+                ),
+                const SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    // Close the screen and return "Yep!" as the result.
+                    Navigator.pop(context, 'Nope! => ${todoArg.title}');
+                  },
+                  child: const Text('Nope!'),
+                )
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
@@ -38,27 +121,4 @@ class Todo {
   final String description;
 
   const Todo(this.title, this.description);
-}
-
-class DetailScreen extends StatelessWidget {
-  // In the constructor, require a Todo.
-  const DetailScreen({super.key, required this.todo});
-
-  // Declare a field that holds the Todo.
-  final Todo todo;
-
-  @override
-  Widget build(BuildContext context) {
-    final todoArg = ModalRoute.of(context)!.settings.arguments as Todo;
-    // Use the Todo to create the UI.
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(todoArg.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Text(todoArg.description),
-      ),
-    );
-  }
 }
